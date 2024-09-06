cfaa028a5be2acbed4b9bbe920cab963cd774d23
Arthur Jen <arthur.jen@commsor.com>
2024-09-06 11:56:22 -0700

    build errors

--- app/page copy.tsx
+++ app/page copy.tsx
@@ -1,4 +1,3 @@
-import Home from "./home"
 import Image from "next/image";
 
 export default function Page() {
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,7 @@ import DiceKingdomBanner from "../public/dice-kingdom.png";
 export default function Page() {
   return (
     <main className="font-[family-name:var(--font-geist-sans)]">
-      <Image
-        src="/dice-kingdom.png"
-        alt="splash image"
-        width="3438"
-        height="1186"
-        priority
-      />
+      <Image src={DiceKingdomBanner} alt="splash image" priority />
       <Home />
     </main>
   );
