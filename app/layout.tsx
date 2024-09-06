3d8fde6a7983c18e8b53a489be8c8a35e3a7de7f
Arthur Jen <arthur.jen@commsor.com>
2024-09-06 14:31:12 -0700

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const shantellSans = localFont({
+  src: "./fonts/ShantellSans.ttf",
+  variable: "--font-shantell-sans",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const patrickHand = localFont({
+  src: "./fonts/patrickHand-Regular.ttf",
+  variable: "--font-patrick-hand",
   weight: "100 900",
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${shantellSans.variable} ${patrickHand.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import DiceKingdomBanner from "../public/dice-kingdom.png";
 
 export default function Page() {
   return (
-    <main className="font-[family-name:var(--font-geist-sans)]">
+    <main className="font-[family-name:var(--font-patrick-hand)]">
       <Image src={DiceKingdomBanner} alt="splash image" priority />
       <Home />
     </main>
