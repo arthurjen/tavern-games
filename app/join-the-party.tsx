91cf51fdecc9cc49ab1b58859393f5fd4a653543
Arthur Jen <arthur.jen@commsor.com>
2024-09-12 13:14:58 -0700

    character placement

--- app/join-the-party.tsx
+++ app/join-the-party.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import DiceKnight from "../public/dice-knight.png";
 import DiceThief from "../public/dice-thief.png";
 import DiceMage from "../public/dice-mage.png";
@@ -7,9 +7,9 @@ import DiceKingdomBanner2 from "../public/dice-kingdom-2.png";
 
 export default function JoinTheParty() {
   return (
-    <div className="flex items-center justify-center w-full text-xl border-main border-b-2">
-      <Image className="" src={DiceMan} alt="Dice Man" width="300" height="100" />
-      <Image src={DiceThief} alt="Dice Thief" width="300" height="100" />
+    <div className="flex relative items-center justify-center w-full text-xl border-main border-b-2">
+      <Character className="left-64" src={DiceMan} alt="Dice Man" />
+      <Character className="left-36 bottom-24" src={DiceThief} alt="Dice Thief" />
 
       <div className="flex flex-col items-center justify-center w-full lg:col-span-3 p-8 lg:p-20">
         <Image
@@ -18,7 +18,7 @@ export default function JoinTheParty() {
           width="300"
           height="100"
         />
-        <div className="flex items-center justify-center flex-col text-center">
+        <div className="flex items-center justify-center flex-col text-center w-72">
           <h1 className=" text-5xl mb-6">Join the party?</h1>
           <p className="text-xl">
             this is a collaborative project and the more the merrier. please hop
@@ -34,8 +34,24 @@ export default function JoinTheParty() {
           </a>
         </div>
       </div>
-      <Image src={DiceMage} alt="Dice Mage" width="300" height="100" />
-      <Image src={DiceKnight} alt="Dice Knight" width="300" height="100" />
+      <Character className="right-64" src={DiceMage} alt="Dice Mage" />
+      <Character className="right-36 bottom-24" src={DiceKnight} alt="Dice Knight" />
     </div>
+  );
+}
+
+function Character(props: {
+  src: StaticImageData;
+  alt: string;
+  className: string;
+}) {
+  return (
+    <Image
+      className={`invisible lg:visible absolute -z-10 ${props.className}`}
+      src={props.src}
+      alt={props.alt}
+      width="200"
+      height="200"
+    />
   );
 }
--- app/video.tsx
+++ app/video.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import YouTube from "react-youtube";
+import YouTube, { YouTubeProps } from "react-youtube";
 export default function Video() {
-  const opts = {
-    height: "390",
-    width: "640",
-  };
-  return <YouTube videoId="2XID_W4neJo" opts={opts} />;
+  // const opts: YouTubeProps["opts"] = {
+  //   height: "390",
+  //   width: "640",
+  // };
+  return <YouTube videoId="2XID_W4neJo" iframeClassName="w-96 h-56 lg:w-full lg:h-full" />;
 }
